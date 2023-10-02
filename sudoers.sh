6baf1ccd66868d4a982cd9872500f70d7cf4e61c
devops-abdullah <66333235+devops-abdullah@users.noreply.github.com>
2023-10-03 00:15:20 +0500

    Update sudoers.sh

--- sudoers.sh
+++ sudoers.sh
@@ -1,7 +1,7 @@
-cat << EOF > /etc/sudoers.d/permission
+sudo cat << EOF > /etc/sudoers.d/permission
 %domain\ admins@vaival.tech ALL=(ALL:ALL) NOPASSWD: ALL
 %domain\ users@vaival.tech ALL=(ALL:ALL) NOPASSWD: /usr/bin/systemctl, /usr/bin/openvpn
 %testgroup@vaival.tech  ALL=(ALL:ALL) NOPASSWD: /usr/bin/systemctl, /usr/bin/openvpn
 EOF
 
-chmod 440 /etc/sudoers.d/permission
+sudo chmod 440 /etc/sudoers.d/permission
