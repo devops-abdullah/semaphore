f471b8028322e95fb87762e2cbf1b99496106915
devops-abdullah <66333235+devops-abdullah@users.noreply.github.com>
2024-03-25 09:55:35 +0000

    docker services check added

---
+++ services_check/scripts/addressbook_check.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+DIR=`docker inspect nginx --format='{{index .Config.Labels "com.docker.compose.project.working_dir"}}'`CONTAINER_NAME="addressbook360"
+PORT="60019"
+
+# Check if the port is listening
+nc -z localhost $PORT
+
+if [ $? -ne 0 ]; then
+    echo "Port $PORT is not listening. Restarting container $CONTAINER_NAME..."
+    	cd $DIR && docker compose up -d --force-recreate $CONTAINER_NAME || docker-compose up -d --force-recreate $CONTAINER_NAME
+else
+    echo "Port $PORT is listening."
+fi
